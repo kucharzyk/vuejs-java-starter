2d091d51cb140501fb08bcb8dba8aa29ee01ac9b
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2016-01-11 21:44:02 +0100

    fixed 404  status code in routing

--- src/main/java/com/shardis/controllers/web/MainPageController.java
+++ src/main/java/com/shardis/controllers/web/MainPageController.java
@@ -2,8 +2,10 @@ package com.shardis.controllers.web;
 
 import com.shardis.utils.EnvironmentProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -17,6 +19,7 @@ public class MainPageController {
     @Autowired
     EnvironmentProvider environmentProvider;
 
+    @ResponseStatus(HttpStatus.OK)
     @RequestMapping("/")
     public ModelAndView mainPage() {
         if (environmentProvider.isProduction()) {
