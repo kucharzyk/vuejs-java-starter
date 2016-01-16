815569ecc8d27b3e2adcd838a876b69021c6274d
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2016-01-16 21:56:29 +0100

    fixed typo

--- src/main/vuejs/components/layout/page-footer.vue
+++ src/main/vuejs/components/layout/page-footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="page-footer footer">
     <div class="text-xs-center container">
-      <span class="text-muted">Vue.js java starter Vopyright &copy; Tomasz Kucharzyk {{new Date().getYear()+1900}}</span>
+      <span class="text-muted">Vue.js java starter Copyright &copy; Tomasz Kucharzyk {{new Date().getYear()+1900}}</span>
     </div>
   </footer>
 </template>
